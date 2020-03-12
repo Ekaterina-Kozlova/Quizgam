75010ee3fd227792dd13f3ad3964e83656cbd49f
Nika_z22 <veronika.sliskova22@yandex.ru>
2020-03-12 15:27:11 +0300

    link title to theme  uuuuaaa

--- app/src/main/java/com/example/quizgame/TitleFragment.kt
+++ app/src/main/java/com/example/quizgame/TitleFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.quizgame.databinding.ActivityMainBinding
 import com.example.quizgame.databinding.TitleFragmentBinding
 
@@ -18,8 +20,13 @@ class TitleFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                                 savedInstanceState: Bundle?): View? {
-        val binding = DataBindingUtil.inflate<TitleFragmentBinding>(
+        //val binding = DataBindingUtil.inflate<TitleFragmentBinding>(
+        //    inflater, R.layout.title_fragment, container, false)
+        val binding:TitleFragmentBinding = DataBindingUtil.inflate(
             inflater, R.layout.title_fragment, container, false)
+        binding.btnStart.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_titleFragment_to_themeFragment)
+        }
         return binding.root
     }
 
